b8af5f8b9752d4eaf7a1d96b95f69f661e8f13d8
Naruto-sudo <64817251+Naruto-sudo@users.noreply.github.com>
2020-11-09 18:39:58 +0900

    Add files via upload

--- music/musicsetup.js
+++ music/musicsetup.js
@@ -1,94 +1,125 @@
-//Tone.js
-var track = []; //各音楽ファイルに割り振られるトラック番号
-var volume = []; //各音楽ファイルに割り振られるボリューム
-var musicnum = 7; //音楽ファイルの総数
+var musicnum = 6; //音楽ファイルの総数
+var player = [];
+var count = 0;
 
-//ここで音楽を取得
-for(var i = 1 ; i < musicnum ; i++ ){
-  volume[i] = new Tone.Volume().toDestination();
-  track[i] = new Tone.Channel().connect(volume[i]);
-  volume.push('');
-  track.push('');
+//メトロノーム
+var countup = function(){
+    console.log(count++);
 }
+setInterval(countup, 1000);
 
-// ファイル名, track名, 感情, 度合
+//音楽リスト
 var music = [
-  ['filename', 'track', 'emotion', 0], //ここは参照しない (musicを取得するときは1からしてね！！！)
-  ['hp_piano', track[1], 'happy', 0],
-  ['hp_strings', track[2], 'happy', 0.3],
-  ['hp_forest', track[3], 'happy', 0.8],
-  ['sd_piano', track[4], 'sad', 0],
-  ['sd_strings', track[5], 'sad', 0.3],
-  ['sd_rain', track[6], 'sad', 0.8]
+  [0, 'url', 'emotion', 0], //ここは参照しない
+  [1, 'hp_piano', 'happy', 0],
+  [2, 'hp_strings', 'happy', 0.3],
+  [3, 'hp_forest', 'happy', 0.8],
+  [4, 'sd_piano', 'sad', 0],
+  [5, 'sd_strings', 'sad', 0.3],
+  [6, 'sd_rain', 'sad', 0.8]
 ];
 
-function makeChannel(url, channel){
-  const player = new Tone.Player({
-    url: `http://127.0.0.1:8887/music/media/${url}.mp3`,
-    loop: true
-  }).sync().start(0);
-  player.connect(channel);
+//プレーヤーの作成
+function makeplayer(number, url){
+    player.push('');
+    player[number] = new Tone.Player({
+        url: `http://127.0.0.1:8888/music/media/${url}.mp3`,
+        autoplay: false,
+        loop: true
+    }).toDestination();
+    
 }
 
-for(var n = 1 ; n < musicnum ; n++ ){
-    makeChannel(music[n][0], music[n][1]);
+for ( var n = 1 ; n < musicnum + 1 ; n++ ){
+    makeplayer(music[n][0], music[n][1]);
+    player[n].mute = true;
+    player[n].stop();
 }
 
 //ここで音楽を再生開始
 var on = false;
 function startmusic(){
     if (on == false) {
-        Tone.Transport.start();
+        for ( var n = 1 ; n < musicnum + 1; n++ ){
+            player[n].start();
+        }
         on = true;
     } else {
-        Tone.Transport.stop();
+        for ( var n = 1 ; n < musicnum + 1; n++ ){
+            player[n].stop();
+        }
         on = false;
     }
 }
 
-//ここで音楽の更新（毎フレーム）
-function soundloop(emotype1, emovalue1, emotype2, emovalue2) {
-    //console.log(emotype1 + " : " + emovalue1);
-    //console.log(emotype2 + " : " + emovalue2);
+//メインここから
+function soundloop(sad, happy) {
+    if ((count % 4000) == 0 ){
+        if (sad > happy){
+            player[1].mute = true;
+            player[2].mute = true;
+            player[3].mute = true;
+            player[4].mute = false;
+            player[5].mute = false;
+            player[6].mute = false;
 
-    if (emovalue1 < emovalue2) {
-        if (emovalue1 >= music[1][3]){
-            volume[1].mute = false;
-        }else{
-            volume[1].mute = true;
+            //piano
+            if (0.2 >= sad){
+                player[4].volume.value = sad * -100;
+            } else if (0.5 >= sad > 0.2){
+                player[4].volume.value = sad * -20;
+            } else if (sad > 0.5){
+                player[4].volume.value = sad * 1;
+            }
+            //strings
+            if (0.2 >= sad){
+                player[5].volume.value = sad * -100;
+            } else if (0.5 >= sad > 0.2){
+                player[5].volume.value = sad * -20;
+            } else if (sad > 0.5){
+                player[5].volume.value = sad * 1;
+            }
+            //ambient
+            if (0.2 >= sad){
+                player[6].volume.value = sad * -100;
+            } else if (0.5 >= sad > 0.2){
+                player[6].volume.value = sad * -10;
+            } else if (sad > 0.5){
+                player[6].volume.value = sad * 1;
+            }
+
+        }else if (happy > sad){
+            player[1].mute = false;
+            player[2].mute = false;
+            player[3].mute = false;
+            player[4].mute = true;
+            player[5].mute = true;
+            player[6].mute = true;
+
+            //piano
+            if (0.2 >= happy){
+                player[1].volume.value = happy * -100;
+            } else if (0.5 >= happy > 0.2){
+                player[1].volume.value = happy * -20;
+            } else if (happy > 0.5){
+                player[1].volume.value = happy * 1;
+            }
+            //strings
+            if (0.2 >= happy){
+                player[2].volume.value = happy * -100;
+            } else if (0.5 >= happy > 0.2){
+                player[2].volume.value = happy * -10;
+            } else if (happy > 0.5){
+                player[2].volume.value = happy * 1;
+            }
+            //ambient
+            if (0.2 >= happy){
+                player[3].volume.value = happy * -100;
+            } else if (0.5 >= happy > 0.2){
+                player[3].volume.value = happy * -10;
+            } else if (happy > 0.5){
+                player[3].volume.value = happy * 1;
+            }
         }
-        if (emovalue1 >= music[2][3]){
-            volume[2].mute = false;
-        }else{
-            volume[2].mute = true;
-        }
-        if (emovalue1 >= music[3][3]){
-            volume[3].mute = false;
-        }else{
-            volume[3].mute = true;
-        }
-        volume[4].mute = true;
-        volume[5].mute = true;
-        volume[6].mute = true;
     }
-    if (emovalue1 > emovalue2) {
-        volume[1].mute = true;
-        volume[2].mute = true;
-        volume[3].mute = true;
-        if (emovalue2 >= music[4][3]){
-            volume[4].mute = false;
-        }else{
-            volume[4].mute = true;
-        }
-        if (emovalue2 >= music[5][3]){
-            volume[5].mute = false;
-        }else{
-            volume[5].mute = true;
-        }
-        if (emovalue2 >= music[6][3]){
-            volume[6].mute = false;
-        }else{
-            volume[6].mute = true;
-        }
-    }
-}
+}    
